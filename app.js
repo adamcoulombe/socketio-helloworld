192e5ce9b04e849c17fea0749824ed4d9230a643
adamcoulombe <adamcoulombe187@hotmail.com>
2012-12-02 10:08:34 -0500

    app updates to support express 3

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-var app = require('express').createServer()
-  , io = require('socket.io').listen(app);
+var app = require('express')()
+  , server = require('http').createServer(app)
+  , io = require('socket.io').listen(server);
 
-app.listen(80);
+var port = process.env.PORT || 80; // Use the port that Heroku provides or default to 5000
+server.listen(port, function() {
+  console.log("Express server listening on port");
+});
+
+
+
 
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
